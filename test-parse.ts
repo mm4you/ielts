1460464c74f9a92dc1671525fea4c88bd402036b
Khang <ungnhutkhang53@gmail.com>
2026-06-20 17:11:23 +0700

    clean: remove temporary test files and unused assets

--- test-parse.ts
+++
@@ -1,3 +0,0 @@
-import { parseMeaning } from './lib/parse';
-console.log(parseMeaning("n   to be everywhere ||| Có mặt ở khắp mọi nơi"));
-console.log(parseMeaning("adj\tproducing a great deal of profit ||| Sinh lời", null));
